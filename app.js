e7863b7bdc0afad8d71fd09b037913f514629af7
David Lee <itxawjteeb@gmail.com>
2017-08-20 22:01:15 -0700

    Divide into separate react components

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="notificationsFrame">
+        <div className="panel">
+          {/* content goes here */}
+        </div>
+      </div>
+    )
+  }
+}
