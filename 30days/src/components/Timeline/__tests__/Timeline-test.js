d966dd85811af16737fb05db21ae0d4fc7f37113
David Lee <itxawjteeb@gmail.com>
2017-09-28 23:38:08 -0700

    Better Testing with Enzyme on Day 25

--- 30days/src/components/Timeline/__tests__/Timeline-test.js
+++ 30days/src/components/Timeline/__tests__/Timeline-test.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
-
+import { shallow, mount } from 'enzyme';
 import Timeline from '../Timeline';
 
 describe('Timeline', () => {
+	let wrapper;
 
 	it('wraps content in a div with .notificationsFrame class', () => {
-	    const wrapper = TestUtils.renderIntoDocument(<Timeline />);
-	    const node =
-	    TestUtils
-	    	.findRenderedDOMComponentWithClass(wrapper, 'notificationsFrame');
+		wrapper = shallow(<Timeline />);
+		expect(wrapper.find('.notificationsFrame').length).toEqual(1);
 	});
 
+	it('has a title of Timeline', () => {
+		wrapper = mount(<Timeline />)
+		expect(wrapper.find('.title').text()).toBe("Timeline")
+	})
+
+	describe('search button', () => {
+		let search;
+		beforeEach(() => wrapper = mount(<Timeline />))
+		beforeEach(() => search = wrapper.find('input.searchInput'))
+
+		it('starts out hidden', () => {
+			expect(search.hasClass('active')).toBeFalsy()
+		})
+		it('becomes visible after being clicked on', () => {
+			const icon = wrapper.find('.searchIcon')
+			icon.simulate('click')
+			expect(search.hasClass('active')).toBeTruthy()
+		})
+	})
+
+	describe('status updates', () => {
+		it('has 4 status updates at minimum', () => {
+			wrapper = shallow(<Timeline />)
+			expect(
+				wrapper.find('ActivityItem').length
+			).toBeGreaterThan(3)
+		})
+	})
 })
